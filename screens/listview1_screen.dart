fe20889aeb6863a97c7d52bbb1377b367f4bda99
FranMa <fmandia1201@gmail.com>
2024-11-04 12:25:37 +0100

    First commit

---
+++ screens/listview1_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  final options = const["TARGARUEB", "STARK", "LANNISTER", "BARATHEON"]
+   
+  const HomeScreen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("LISTVIEW TIPO 1"),
+      ),
+      body: ListView(
+        children: const [
+          ListTile(
+            leading: Icon( Icons.abc_rounded),
+            title: Text("HOLA MUNDO"),
+          ),
+          Text("HOLA MUNDO"),
+          Text("HOLA MUNDO"),
+          Text("HOLA MUNDO")
+        ],
+      ));
+  }
+}
